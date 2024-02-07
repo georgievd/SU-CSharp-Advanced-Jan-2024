8b84752fd253d055841a4587d8f8217e8e06e7d0
Doncho Georgiev <46848927+georgievd@users.noreply.github.com>
2024-02-07 21:21:00 +0200

    Add Generics exercises

---
+++ Generics/Generics/Generics/Box.cs
@@ -0,0 +1,44 @@
+﻿using System.Reflection;
+using System.Text;
+
+namespace Generics
+{
+    public class Box<T> where T : IComparable
+    {
+        private List<T> list;
+
+        public Box()
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public int Count(T item)
+        {
+            int count = 0;
+            foreach (var listItem in list)
+            {
+                if (listItem.CompareTo(item) > 0)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            foreach (var item in list)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ Generics/Generics/Generics/StartUp.cs
@@ -0,0 +1,120 @@
+﻿using System.Globalization;
+
+namespace Generics
+{
+    public class Startup
+    {
+        static void Main()
+        {
+            // GenericBoxInts();
+            // CallGenericSwap();
+            // DoubleComparison();
+            // ExerciseTuple();
+
+            string[] nameTokens = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] beerTokens = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] bankTokens = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string townName = nameTokens.Length == 5 ? $"{nameTokens[3]} {nameTokens[4]}" : nameTokens[3];
+
+
+            Threeuple<string, string, string> names = new($"{nameTokens[0]} {nameTokens[1]}", nameTokens[2], townName);
+            Threeuple<string, int, bool> beers = new(beerTokens[0], int.Parse(beerTokens[1]),
+                beerTokens[2] == "drunk");
+            Threeuple<string, double, string> account = new(bankTokens[0],
+                double.Parse(bankTokens[1], CultureInfo.InvariantCulture), bankTokens[2]);
+
+            Console.WriteLine(names);
+            Console.WriteLine(beers);
+            Console.WriteLine(account);
+        }
+
+        private static void ExerciseTuple()
+        {
+            string[] nameTokens = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] beerTokens = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            string[] numberTokens = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string townName;
+
+            CustomTuple<string, string> names = new($"{nameTokens[0]} {nameTokens[1]}", nameTokens[2]);
+            CustomTuple<string, int> beers = new(beerTokens[0], int.Parse(beerTokens[1]));
+            CustomTuple<int, double> numbers = new(int.Parse(numberTokens[0]),
+                double.Parse(numberTokens[1], CultureInfo.InvariantCulture));
+
+            Console.WriteLine(names);
+            Console.WriteLine(beers);
+            Console.WriteLine(numbers);
+        }
+
+        private static void DoubleComparison()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            Box<double> myBox = new();
+
+            for (int i = 0; i < lines; i++)
+            {
+                myBox.Add(double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture));
+            }
+            double comapreWith = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine(myBox.Count(comapreWith));
+        }
+
+
+        private static void CallGenericSwap()
+        {
+            int lines = int.Parse(Console.ReadLine());
+            List<int> list = new();
+            for (int i = 0; i < lines; i++)
+            {
+                list.Add(int.Parse(Console.ReadLine()));
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            GenericSwap(list, indexes[0], indexes[1]);
+
+            foreach (var listItem in list)
+            {
+                Console.WriteLine($"{listItem.GetType()}: {listItem}");
+            }
+        }
+
+        private static void GenericBoxInts()
+        {
+            int lines = int.Parse(Console.ReadLine());
+
+            Box<int> intBox = new Box<int>();
+            for (int i = 0; i < lines; i++)
+            {
+                intBox.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.WriteLine(intBox);
+        }
+
+        public static void GenericSwap<T>(List<T> items, int firstIndex, int secondIndex)
+        {
+            // Option 1: 
+            //T temp = items[firstIndex];
+            //items[firstIndex] = items[secondIndex];
+            //items[secondIndex] = temp;
+
+            // Option 2:
+            (items[firstIndex], items[secondIndex]) = (items[secondIndex], items[firstIndex]);
+        }
+    }
+}
